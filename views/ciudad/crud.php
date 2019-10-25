41056c573f2b1a45398e83f2cdcb8936767dc81f
kmilo273 <camiloandressuarezp@hotmail.com>
2019-10-25 18:53:50 -0500

    xD

--- views/ciudad/crud.php
+++ views/ciudad/crud.php
@@ -30,8 +30,8 @@
         <!-- Formulario De Registro -->
         <?php if (!isset($_GET['id'])) : ?>
           <form action="<?= baseUrl ?>ciudad/registrar" method="POST">
-          <h2>Registro</h2>
-          <hr>
+            <h2>Registro</h2>
+            <hr>
             <div class="row">
               <div class="form-group col-12">
                 <label for="nombre">Nombre</label>
@@ -62,13 +62,17 @@
           </form>
         <?php else : ?>
           <!-- Formulario para Editar -->
+
+
           <form action="<?= baseUrl ?>ciudad/actualizar&id=<?= $_GET['id'] ?>" method="POST">
+            <h3>Editar</h3>
+            <hr>
             <div class="row">
-              <div class="form-group col-6">
+              <div class="form-group col-12">
                 <label for="nombre">Nombre</label>
                 <input id="nombre" name="nombre" class="form-control" type="text" value="<?= $data->nombreCiudad ?>">
               </div>
-              <div class="col-6">
+              <div class="col-12">
                 <span>Clima</span><br>
                 <select class="custom-select mr-sm-2 mt-1" name="clima" id="clima">
                   <option>Elija...</option>
@@ -78,7 +82,7 @@
                   <?php endwhile; ?>
                 </select>
               </div>
-              <div class="col-6">
+              <div class="col-12">
                 <span>País</span><br>
                 <select class="custom-select mr-sm-2 mt-1" name="pais" id="pais">
                   <option>Elija...</option>
@@ -91,36 +95,37 @@
               <input type="submit" class="btn btn-primary btn-sm btn-block col-6 offset-3 my-3" value="Actualizar">
             </div>
           </form>
-        <?php endif; ?>
+
       </div>
-      <div class="col-9">
-        <!-- Tabla -->
-        <table id="papitas" class="table table-responsive-lg table-bordered table-hover">
-          <thead class="bg-dark">
+    <?php endif; ?>
+    <div class="col-9">
+      <!-- Tabla -->
+      <table id="papitas" class="table table-responsive-lg table-bordered table-hover">
+        <thead class="bg-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Clima</th>
+            <th scope="col">Pais</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($c = $ciudades->fetch_object()) : ?>
             <tr>
-              <th scope="col">Nombre</th>
-              <th scope="col">Clima</th>
-              <th scope="col">Pais</th>
-              <th scope="col">Fecha</th>
-              <th scope="col">Acciones</th>
+              <td><?= $c->nombreCiudad ?></td>
+              <td><?= $c->nombreClima ?></td>
+              <td><?= $c->nombrePais ?></td>
+              <td><?= $c->fecha ?></td>
+              <td class="d-flex justify-content-around">
+                <a href="<?= baseUrl ?>ciudad/eliminar&id=<?= $c->idCiudad ?>" class="btn btn-danger btn-sm">Eliminar</a>
+                <a href="<?= baseUrl ?>ciudad/gestion&id=<?= $c->idCiudad ?>" class="btn btn-info btn-sm">Editar</a>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            <?php while ($c = $ciudades->fetch_object()) : ?>
-              <tr>
-                <td><?= $c->nombreCiudad ?></td>
-                <td><?= $c->nombreClima ?></td>
-                <td><?= $c->nombrePais ?></td>
-                <td><?= $c->fecha ?></td>
-                <td class="d-flex justify-content-around">
-                  <a href="<?= baseUrl ?>ciudad/eliminar&id=<?= $c->idCiudad ?>" class="btn btn-danger btn-sm">Eliminar</a>
-                  <a href="<?= baseUrl ?>ciudad/gestion&id=<?= $c->idCiudad ?>" class="btn btn-info btn-sm">Editar</a>
-                </td>
-              </tr>
-            <?php endwhile ?>
-          </tbody>
-        </table>
-      </div>
+          <?php endwhile ?>
+        </tbody>
+      </table>
+    </div>
     </div>
 
 
